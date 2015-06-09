5c776ad29c49f77a22cce4dbf5057c4de11f8ecc
knuthelgesen <knut.helgesen@bouvet.no>
2015-06-09 21:56:19 +0200

    Make sure user authenticates, otherwise close connection

--- order-front/src/main/java/no/plasmid/order/websocket/OrderEndPoint.java
+++ order-front/src/main/java/no/plasmid/order/websocket/OrderEndPoint.java
@@ -1,5 +1,7 @@
 package no.plasmid.order.websocket;
 
+import java.io.IOException;
+
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -19,13 +21,15 @@ public class OrderEndPoint {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderEndPoint.class);
 	
-	private static final long AUTHENTICATION_TIME_LIMIT	= 1000L;
+	private static final long AUTHENTICATION_TIME_LIMIT	= 5000L;
 
 	private static final String USER_PROPERTY_USER	= "user";
 	
 	@OnOpen
 	public void onOpen(Session session) {
 		LOGGER.debug("Connection open");
+
+		new CheckAuthenticationThread(session).start();
 	}
 	
 	@OnMessage
@@ -47,6 +51,44 @@ public class OrderEndPoint {
 			LOGGER.debug("Removing adapter for user " + user.getUserName());
 			WebsocketAdapterRepository.getInstance().removeAdapter(user);
 		}
+	}
+	
+	/*
+	 * Used to ensure that user authenticates within the set time limit
+	 */
+	private class CheckAuthenticationThread extends Thread {
+		private String LOCK	= "LOCK";
+	
+		private final Session session;
+		
+		public CheckAuthenticationThread(Session session) {
+			this.session = session;
+		}
+	
+		@Override
+		public void run() {
+			//Wait for the time limit
+			synchronized (LOCK) {
+				try {
+					LOCK.wait(AUTHENTICATION_TIME_LIMIT);
+				} catch (InterruptedException e) {
+					LOGGER.error(e.getMessage(), e);
+				}
+			}
+			
+			//Check that the user is authenticated
+			if (null == session.getUserProperties().get(USER_PROPERTY_USER) && session.isOpen()) {
+				try {
+					//No? Then close the session
+					LOGGER.debug("User not authenticated. Will close session");
+					session.close();
+				} catch (IOException e) {
+					LOGGER.error(e.getMessage(), e);
+				}
+			}
+			
+		}
+		
 	}
 	
 }
